a9d22ce6d280dbd5240931b791c16a7f39286e59
Daniel.Dawson <daniel.dawson@skybettingandgaming.com>
2023-01-13 17:00:57 +0000

    web-crawler - tidied up the main function and arranged imports

---
+++ worker/worker.go
@@ -0,0 +1,34 @@
+package worker
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/harshmist/web-crawler/input"
+	"github.com/harshmist/web-crawler/links"
+)
+
+func InitWorker() {
+	for {
+		input, err := input.InputReciever()
+		if err != nil || strings.ToUpper(input) == "EXIT" {
+			break
+		}
+		resp, err := http.Get(input)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		list, err := links.LinkFinder(resp)
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		fmt.Print(list)
+		resp.Body.Close()
+	}
+}
